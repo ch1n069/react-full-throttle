2f621007b2d4bdc6a5d08b3ff36c856cf2efc1c6
Bruno Alpachino <brunolalpachino@gmail.com>
2023-07-08 11:29:24 +0300

    created context for the app

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,2 +1,6 @@
 // we use kebab case for the class names in areas
 // we dont store components
+import React from "react";
+const AuthContext = React.createContext({ isLoggedin: false });
+
+export default AuthContext;
